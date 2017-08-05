5533e4e7bde72351ed527336e56b32f91c782f3c
renatn <zen@live.ru>
2017-08-05 20:49:22 +0300

    Initial import

---
+++ index.js
@@ -0,0 +1,65 @@
+const sax = require('sax');
+const fs = require('fs');
+const express = require('express')
+const app = express()
+const fileUpload = require('express-fileupload');
+const stream = require('stream');
+const mustacheExpress = require('mustache-express');
+
+let dot = 'digraph G { ';
+let state;
+
+
+const handleErrorParse =  e => {
+  console.error("error!", e)
+};
+
+const handleStartTag = node => {
+  if (node.name === 'state') {
+ 	const { name } = node.attributes;
+	state = name;
+	dot = dot + ' ' + name + '; ';
+  } else if (node.name === 'state-transition') {
+ 	const { name, to } = node.attributes;
+  	dot = dot + state + ' -> ' + to + ' [label=' + name + ']; ';
+  }
+};
+
+const endParseHandler = (req, res) => e => {
+	dot = dot + '}';
+	// res.status(200).send(dot);
+	res.render('graph.html', { dotString: dot });
+}
+
+const handleUpload = (req, res) => {
+	if (!req.files || !req.files.xml) {
+		return res.status(400).send('No XML files were upload');
+	}
+
+	dot = 'digraph G { ';
+	state = '';
+
+	const handleEndParse = endParseHandler(req, res);
+
+	const saxStream = sax.createStream(true)
+	saxStream.on("error", handleErrorParse);
+	saxStream.on("opentag", handleStartTag);
+	saxStream.on('end', handleEndParse);
+
+	const dataStream = new stream.PassThrough();
+	dataStream.end(req.files.xml.data);
+	dataStream.pipe(saxStream);
+}
+
+
+app.use(express.static('public'));
+app.use(fileUpload());
+app.engine('html', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.post('/render', handleUpload);
+
+app.listen(3000, function () {
+  console.log('UFS Workflow Viz app listening on port 3000!')
+})
